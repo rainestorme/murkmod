501cc85ba380e568a2e04807caec7a85bbb980e6
rainestorme <115757568+rainestorme@users.noreply.github.com>
2023-03-15 13:40:35 -0500

    Add fakemurk-daemon.sh with removed telemetry and autoupdates

---
+++ fakemurk-daemon.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+{
+    until tpm_manager_client take_ownership; do
+        echo "failed to take ownership"
+        sleep 0.5
+    done
+
+    {
+        launch_racer(){
+            echo launching racer at "$(date)"
+            {
+                while true; do
+                    cryptohome --action=remove_firmware_management_parameters >/dev/null 2>&1
+                done
+            } &
+            RACERPID=$!
+        }
+        launch_racer
+        while true; do
+            echo "checking cryptohome status"
+            if [ "$(cryptohome --action=is_mounted)" == "true" ]; then
+                if ! [ -z $RACERPID ]; then
+                    echo "logged in, waiting to kill racer"
+                    sleep 60
+                    kill -9 $RACERPID
+                    echo "racer terminated at $(date)"
+                    RACERPID=
+                fi
+            else
+                if [ -z $RACERPID ]; then 
+                    launch_racer
+                fi
+            fi
+            sleep 10
+        done
+    } &
+
+    {
+        while true; do
+            vpd -i RW_VPD -s check_enrollment=0 >/dev/null 2>&1
+            vpd -i RW_VPD -s block_devmode=0 >/dev/null 2>&1
+            crossystem.old block_devmode=0 >/dev/null 2>&1
+            sleep 60
+        done
+    } &
+} &
+
+{
+    while true; do
+        if test -d "/home/chronos/user/Downloads/disable-extensions"; then
+            kill -9 $(pgrep -f "\-\-extension\-process") 2>/dev/null
+            sleep 0.5
+        else
+            sleep 5
+        fi
+    done
+} &
