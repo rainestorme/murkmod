b385aeafb909c610a0ed9e162dc486855b5508b5
rainestorme <115757568+rainestorme@users.noreply.github.com>
2023-03-18 15:49:43 -0500

    Update helloworld.sh

--- plugins/helloworld.sh
+++ plugins/helloworld.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 PLUGIN_NAME="Hello World Plugin"
+PLUGIN_FUNCTION="Print hello world to the console"
 PLUGIN_DESCRIPTION="The friendliest murkmod plugin you'll ever see."
 PLUGIN_AUTHOR="rainestorme"
 PLUGIN_VERSION=1
