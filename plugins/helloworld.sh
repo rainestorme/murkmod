91368dfb9a71ed215dbb26a8c6a6b7b9a25510a3
rainestorme <115757568+rainestorme@users.noreply.github.com>
2023-03-18 13:50:13 -0500

    Add $PLUGIN_DESCRIPTION

--- plugins/helloworld.sh
+++ plugins/helloworld.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 $PLUGIN_NAME="Hello World Plugin"
+$PLUGIN_DESCRIPTION="The friendliest murkmod plugin you'll ever see."
 $PLUGIN_AUTHOR="rainestorme"
 $PLUGIN_VERSION=1
 echo "Hello, World!"
