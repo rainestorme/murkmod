a02d5f69e9d346b580c960e1f0d00b1c3c061a63
rainestorme <115757568+rainestorme@users.noreply.github.com>
2023-03-21 13:20:34 -0500

    Accidentally broke mush

--- mush.sh
+++ mush.sh
@@ -174,7 +174,7 @@ install_plugins() {
     local plugin_url="$plugins_url/$plugin_name"
     local plugin_info=$(curl -s $plugin_url)
 
-    if [[ grep "Not Found" <<< $plugin_info ]]; then
+    if [[ $plugin_info == *"Not Found"* ]]; then
       echo "Plugin not found"
     else
       local plugin_file_url=$(echo "$plugin_info" | jq -r '.download_url')
