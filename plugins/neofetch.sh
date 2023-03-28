20836155afca353ce3f5453565741703db96d708
rainestorme <115757568+rainestorme@users.noreply.github.com>
2023-03-28 08:50:47 -0500

    Update neofetch.sh

--- plugins/neofetch.sh
+++ plugins/neofetch.sh
@@ -3,6 +3,8 @@ PLUGIN_NAME="Neofetch"
 PLUGIN_FUNCTION="Run Neofetch"
 PLUGIN_DESCRIPTION="Originally built-in to murkmod, now a plugin. Take screenshots on your Chromebook in style!"
 PLUGIN_AUTHOR="rainestorme"
-PLUGIN_VERSION=1
+PLUGIN_VERSION=2
 
 curl https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch | bash
+
+read -n 1 -s -r -p "Press any key to continue"
