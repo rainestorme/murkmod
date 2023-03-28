0a4d1768f518360a53a858a58416c9742c4830f1
rainestorme <115757568+rainestorme@users.noreply.github.com>
2023-03-28 08:34:21 -0500

    Refactor and increment version

--- plugins/mrchromebox-fwscript.sh
+++ plugins/mrchromebox-fwscript.sh
@@ -3,10 +3,16 @@ PLUGIN_NAME="MrChromebox Firmware Utility Script"
 PLUGIN_FUNCTION="Run MrChromebox's firmware utility script"
 PLUGIN_DESCRIPTION="Runs MrChromebox's firmware utility script."
 PLUGIN_AUTHOR="MrChromebox, DiffuseHyperion"
-PLUGIN_VERSION=1
+PLUGIN_VERSION=2
 
 doas() {
     ssh -t -p 1337 -i /rootkey -oStrictHostKeyChecking=no root@127.0.0.1 "$@"
 }
 
-cd; curl -LOk mrchromebox.tech/firmware-util.sh && doas "cd; bash firmware-util.sh"
+pushd ~
+    curl -LOk mrchromebox.tech/firmware-util.sh
+    doas "pushd ~
+    bash firmware-util.sh
+    popd
+    exit"
+popd
