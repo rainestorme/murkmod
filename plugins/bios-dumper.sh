d5be63026ca5f4006da575f310ce00f6f96ea030
rainestorme <115757568+rainestorme@users.noreply.github.com>
2023-03-29 09:48:21 -0500

    Prettify bios-dumper output

--- plugins/bios-dumper.sh
+++ plugins/bios-dumper.sh
@@ -3,17 +3,18 @@ PLUGIN_NAME="BIOS Dumper"
 PLUGIN_FUNCTION="Dump current BIOS"
 PLUGIN_DESCRIPTION="Dumps the current system firmware/BIOS for further reverse-engineering"
 PLUGIN_AUTHOR="rainestorme"
-PLUGIN_VERSION=1
+PLUGIN_VERSION=2
 
 doas() {
     ssh -t -p 1337 -i /rootkey -oStrictHostKeyChecking=no root@127.0.0.1 "$@"
 }
 
-echo "Dumping firmware..."
 doas "pushd /home/chronos/user/Downloads
 clear
+echo 'Dumping firmware...'
 flashrom -r bios.bin &> /dev/null
 popd
 exit"
+clear
 echo "Done! Look in /home/chronos/user/Downloads (just labeled 'Downloads' in the file explorer) for the .bin file."
 sleep 1
