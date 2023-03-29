cb931cc8b2b2b11c2fec318d42675cc1c680d89c
rainestorme <115757568+rainestorme@users.noreply.github.com>
2023-03-29 09:38:41 -0500

    Clear screen before opening gbb-tools

--- plugins/gbb-tools.sh
+++ plugins/gbb-tools.sh
@@ -3,7 +3,7 @@ PLUGIN_NAME="GBB Tools"
 PLUGIN_FUNCTION="Open GBB tools"
 PLUGIN_DESCRIPTION="Allows you to view, edit, and remove GBB (Google Binary Block) flags on the system easily"
 PLUGIN_AUTHOR="kubisnax, rainestorme"
-PLUGIN_VERSION=1
+PLUGIN_VERSION=2
 
 doas() {
     ssh -t -p 1337 -i /rootkey -oStrictHostKeyChecking=no root@127.0.0.1 "$@"
@@ -12,6 +12,7 @@ doas() {
 pushd /tmp
     curl -LOk https://raw.githubusercontent.com/kubisnax/gbb_tools/master/gbbtools.sh
     doas "pushd /tmp
+    clear
     bash gbbtools.sh
     popd
     exit"
