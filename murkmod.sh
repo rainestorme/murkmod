25b867db23e05b24c78cb4b65213921676288487
rainestorme <115757568+rainestorme@users.noreply.github.com>
2023-03-29 09:06:14 -0500

    Update murkmod.sh

--- murkmod.sh
+++ murkmod.sh
@@ -75,9 +75,9 @@ do_policy_patch() {
     else
         read -r -p "Use murkmod reccomended pollen config? [Y/n] " choice
         case "$choice" in
-                1) install "pollen.json" /etc/opt/chrome/policies/managed/policy.json ;;
-                2) install_fakemurk "pollen.json" /etc/opt/chrome/policies/managed/policy.json ;;
-                *) install "pollen.json" /etc/opt/chrome/policies/managed/policy.json ;;
+        1) install "pollen.json" /etc/opt/chrome/policies/managed/policy.json ;;
+        2) install_fakemurk "pollen.json" /etc/opt/chrome/policies/managed/policy.json ;;
+        *) install "pollen.json" /etc/opt/chrome/policies/managed/policy.json ;;
         esac
     fi
 }
