583889d3529297df24323439a66c0ebcbb8f2767
rainestorme <115757568+rainestorme@users.noreply.github.com>
2023-03-29 09:37:23 -0500

    Add gbb tools plugin

---
+++ plugins/gbb-tools.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+PLUGIN_NAME="GBB Tools"
+PLUGIN_FUNCTION="Open GBB tools"
+PLUGIN_DESCRIPTION="Allows you to view, edit, and remove GBB (Google Binary Block) flags on the system easily"
+PLUGIN_AUTHOR="kubisnax, rainestorme"
+PLUGIN_VERSION=1
+
+doas() {
+    ssh -t -p 1337 -i /rootkey -oStrictHostKeyChecking=no root@127.0.0.1 "$@"
+}
+
+pushd /tmp
+    curl -LOk https://raw.githubusercontent.com/kubisnax/gbb_tools/master/gbbtools.sh
+    doas "pushd /tmp
+    bash gbbtools.sh
+    popd
+    exit"
+popd
