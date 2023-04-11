24bfc7dcc4479e99ebce2b3e83fc2a1b74bf29bc
rainestorme <115757568+rainestorme@users.noreply.github.com>
2023-04-11 13:33:33 -0500

    Finish wssocks plugin

--- plugins/wssocks.sh
+++ plugins/wssocks.sh
@@ -3,15 +3,20 @@ PLUGIN_NAME="wssocks"
 PLUGIN_FUNCTION="Manage proxy connections"
 PLUGIN_DESCRIPTION="Allows you to connect to and use wssocks proxies - socks5 over websockets instead of traditional socks5 TCP connections (which could be blocked)"
 PLUGIN_AUTHOR="genshen, rainestorme"
-PLUGIN_VERSION=1
+PLUGIN_VERSION=2
 
 doas() {
     ssh -t -p 1337 -i /rootkey -oStrictHostKeyChecking=no root@127.0.0.1 "$@"
 }
 
-echo "This plugin is a work-in-progress - by which I mean that it does not work. Don't use this in production."
+echo "This plugin is a work-in-progress. Don't use this in production (or for anything critical)."
 
 pushd /tmp
+    echo "Enter the wss:// or ws:// url to a wssocks proxy, or leave this field blank to use a hosted one (on replit, check the status here: https://10cf60a3-1599-4cc6-a7b5-db06769a323e.id.repl.co/status/)"
+    read -p ' > ' wssocks_host
+    if [ -z "$wssocks_host" ]; then
+        wssocks_host="wss://10cf60a3-1599-4cc6-a7b5-db06769a323e.id.repl.co"
+    fi
     echo "Checking architecture..."
     architecture=""
     case $(uname -m) in
@@ -27,6 +32,8 @@ pushd /tmp
     echo 'Done!'
     clear
     ./$filename version
+    echo 'Starting wssocks client...'
+    ./$filename client --addr :1080 --remote $wssocks_host --http & echo 'Up and running! Point your Chromebook\'s proxy settings (socks5) to :1080 to connect. Press Ctrl+C to exit.'
     popd
     exit"
 popd
