5c2fbd1ed25c3a224ef35c8aa31666cf10860251
rainestorme <115757568+rainestorme@users.noreply.github.com>
2023-11-01 14:26:18 -0500

    Create stateful_cleaner.sh

---
+++ plugins/stateful_cleaner.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# menu_plugin
+PLUGIN_NAME="Stateful Cleaner"
+PLUGIN_FUNCTION="Clean stateful"
+PLUGIN_DESCRIPTION="Cleans up unneccecary files on stateful to free up space"
+PLUGIN_AUTHOR="rainestorme"
+PLUGIN_VERSION=0
+
+doas() {
+    ssh -t -p 1337 -i /rootkey -oStrictHostKeyChecking=no root@127.0.0.1 "$@"
+}
+
+doas "rm -Rf /mnt/stateful_partition/cros_sign_backups
+rm -Rf /mnt/stateful_partition/unencrypted/apkcache/*
+rm -Rf /mnt/stateful_partition/unencrypted/cache/vpd/*"
