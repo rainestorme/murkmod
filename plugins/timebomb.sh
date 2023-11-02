dee8986ba25bc6377afaa922737367b19e1fdbb4
rainestorme <115757568+rainestorme@users.noreply.github.com>
2023-11-02 10:53:41 -0500

    Update timebomb.sh

--- plugins/timebomb.sh
+++ plugins/timebomb.sh
@@ -6,13 +6,19 @@ PLUGIN_DESCRIPTION="Automatically reverts your Chromebook if the date is past a 
 PLUGIN_AUTHOR="rainestorme"
 PLUGIN_VERSION=1
 
-echo "Checking timebomb status..."
-today=$(date +%Y-%m-%d)
-enddate="2024-05-12" # >>> Change this date after installing the plugin!
-
 doas() {
     ssh -t -p 1337 -i /rootkey -oStrictHostKeyChecking=no root@127.0.0.1 "$@"
 }
+
+echo "Checking timebomb status..."
+if [ ! -f /mnt/stateful_partition/murkmod/timebomb ]; then
+    echo "Setting timebomb for one year..."
+    doas "touch /mnt/stateful_partition/murkmod/timebomb"
+    doas "date +%Y-%m-%d --date='next year' > /mnt/stateful_partition/murkmod/timebomb" # >>> Change this date after installing the plugin!
+fi
+    
+today=$(date +%Y-%m-%d)
+enddate=$(cat /mnt/stateful_partition/murkmod/timebomb)
 
 if [[ "$today" > "$enddate" ]]; then
     if [ ! -f /no-timebomb ]; then
