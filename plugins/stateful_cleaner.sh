20fdf43709957207f7c1dc5e014fb720ebe2db86
rainestorme <115757568+rainestorme@users.noreply.github.com>
2023-11-20 18:37:02 -0600

    Update Stateful Cleaner to also delete emergency revert backups

--- plugins/stateful_cleaner.sh
+++ plugins/stateful_cleaner.sh
@@ -12,4 +12,6 @@ doas() {
 
 doas "rm -Rf /mnt/stateful_partition/cros_sign_backups
 rm -Rf /mnt/stateful_partition/unencrypted/apkcache/*
-rm -Rf /mnt/stateful_partition/unencrypted/cache/vpd/*"
+rm -Rf /mnt/stateful_partition/unencrypted/cache/vpd/*
+rm -f /mnt/stateful_partition/murkmod/kern_backup.img
+rm -f /mnt/stateful_partition/murkmod/root_backup.img"
