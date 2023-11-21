960edfd96e5a7887880c6656eb2fea158e8d0945
rainestorme <115757568+rainestorme@users.noreply.github.com>
2023-11-21 09:59:05 -0600

    Add Bootsplasher plugin (#35)

---
+++ plugins/bootsplasher.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# menu_plugin
+PLUGIN_NAME="Bootsplasher"
+PLUGIN_FUNCTION="Set custom boot splash"
+PLUGIN_DESCRIPTION="Simple plugin to set a custom boot splash from the user's downloads folder"
+PLUGIN_AUTHOR="rainestorme"
+PLUGIN_VERSION=1
+
+doas() {
+    ssh -t -p 1337 -i /rootkey -oStrictHostKeyChecking=no root@127.0.0.1 "$@"
+}
+
+echo "Make sure your bootsplash is in PNG format!"
+echo "Currently, only static boot splashes are supported. Up to 30 frames of animation (1 second) can be added manually, and through this plugin in the future."
+echo "Select an option:"
+echo " 1. Set custom static bootsplash"
+echo " 2. Restore murkmod default bootsplash"
+read -p '> ' choice
+
+case $variable in
+    "1" ) set_custom ;;
+    "2" ) restore_murkmod ;;
+    * ) echo "Invalid choice." ;;
+esac
+
+copy_bootsplash_static() {
+  echo "Copying bootsplash..."
+  for i in $(seq -f "%02g" 0 30); do
+    cp /tmp/bootsplash.png /usr/share/chromeos-assets/images_100_percent/boot_splash_frame${i}.png
+  done
+  rm -f /tmp/bootsplash.png
+  echo "Done!"
+}
+
+get_asset() {
+    curl -s -f "https://api.github.com/repos/rainestorme/murkmod/contents/$1" | jq -r ".content" | base64 -d
+}
+
+install() {
+    TMP=$(mktemp)
+    get_asset "$1" >"$TMP"
+    if [ "$?" == "1" ] || ! grep -q '[^[:space:]]' "$TMP"; then
+        echo "Failed to install $1 to $2"
+        rm -f "$TMP"
+        exit
+    fi
+    # Don't mv, that would break permissions
+    cat "$TMP" >"$2"
+    rm -f "$TMP"
+}
+
+set_custom() {
+  read -p 'Enter filename (downloads folder) > ' bootsplash
+  cp $bootsplash /tmp/bootsplash.png
+  copy_bootsplash_static
+}
+
+restore_murkmod() {
+  echo "Grabbing murkmod bootsplash..."
+  install "chromeos-bootsplash-v2.png" /tmp/bootsplash.png
+  copy_bootsplash_static
+}
