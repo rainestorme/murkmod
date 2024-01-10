7b1e0c2dae6ab521c0684bcdd760d19e7b47d145
rainestorme <115757568+rainestorme@users.noreply.github.com>
2024-01-10 16:23:15 -0600

    Create test.js

---
+++ plugins/test.js
@@ -0,0 +1,13 @@
+const PLUGIN_NAME = "test.js";
+const PLUGIN_FUNCTION = "test js plugin";
+const PLUGIN_DESCRIPTION = "testt"
+const PLUGIN_AUTHOR = "rainestorme";
+const PLUGIN_VERSION = 1;
+
+function plugin_init() {
+  console.log("js init!!!1!");
+}
+
+function plugin_main() {
+  alert("under construction!");
+}
