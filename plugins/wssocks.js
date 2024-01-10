091629235df5257ac235f26e930fc0c60e8fa0b5
rainestorme <115757568+rainestorme@users.noreply.github.com>
2024-01-10 16:21:59 -0600

    Update wssocks.js

--- plugins/wssocks.js
+++ plugins/wssocks.js
@@ -1,6 +1,6 @@
 const PLUGIN_NAME = "wssocks (js)";
 const PLUGIN_FUNCTION = "Open wssocks menu";
-const PLUGIN_DESCRIPTION = "SOCKS5 over websockets
+const PLUGIN_DESCRIPTION = "SOCKS5 over websockets"
 const PLUGIN_AUTHOR = "rainestorme";
 const PLUGIN_VERSION = 1;
 
