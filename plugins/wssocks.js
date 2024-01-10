305ce5819079df6962166714c98b71c4ddd34233
rainestorme <115757568+rainestorme@users.noreply.github.com>
2024-01-10 14:38:22 -0600

    Add placeholder js plugin

---
+++ plugins/wssocks.js
@@ -0,0 +1,13 @@
+const PLUGIN_NAME = "wssocks (js)";
+const PLUGIN_FUNCTION = "Open wssocks menu";
+const PLUGIN_DESCRIPTION = "SOCKS5 over websockets
+const PLUGIN_AUTHOR = "rainestorme";
+const PLUGIN_VERSION = 1;
+
+function plugin_init() {
+  console.log("wssocks init!!!1!");
+}
+
+function plugin_main() {
+  alert("under construction!");
+}
