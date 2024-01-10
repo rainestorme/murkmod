5e15bd63bfda8f7fca7f2688fa0cb64cc84f26cd
rainestorme <115757568+rainestorme@users.noreply.github.com>
2024-01-10 16:24:32 -0600

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,8 +1,8 @@
-const PLUGIN_NAME = "test.js";
-const PLUGIN_FUNCTION = "test js plugin";
-const PLUGIN_DESCRIPTION = "testt"
-const PLUGIN_AUTHOR = "rainestorme";
-const PLUGIN_VERSION = 1;
+var PLUGIN_NAME = "test.js";
+var PLUGIN_FUNCTION = "test js plugin";
+var PLUGIN_DESCRIPTION = "testt"
+var PLUGIN_AUTHOR = "rainestorme";
+var PLUGIN_VERSION = 1;
 
 function plugin_init() {
   console.log("js init!!!1!");
