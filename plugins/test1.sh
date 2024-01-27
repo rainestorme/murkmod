54d4df7287d9ec4664d37b2fd94d2b411d6748ba
rainestorme <115757568+rainestorme@users.noreply.github.com>
2024-01-27 20:48:08 +0000

    Test plugins

---
+++ plugins/test1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# menu_plugin
+PLUGIN_NAME="Hello World Plugin"
+PLUGIN_FUNCTION="Print a hello world message"
+PLUGIN_DESCRIPTION="The friendliest murkmod plugin you'll ever see."
+PLUGIN_AUTHOR="rainestorme"
+PLUGIN_VERSION=1
+PLUGIN_DEPENDENCIES=("test2.sh")
+echo "Hello, World!"
---
+++ plugins/test2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# menu_plugin
+PLUGIN_NAME="Hello World Plugin (dependency, do not use)"
+PLUGIN_FUNCTION="Print a hello world message"
+PLUGIN_DESCRIPTION="The friendliest murkmod plugin you'll ever see."
+PLUGIN_AUTHOR="rainestorme"
+PLUGIN_VERSION=1
+echo "Hello, World!"
