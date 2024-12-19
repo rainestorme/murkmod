d44a20b9b25ac986beb57e7fdc5943b7fb5ea82b
rainestorme <115757568+rainestorme@users.noreply.github.com>
2024-12-19 09:12:13 -0600

    Remove analytics since the backend has been broken for a while

--- murkmod.sh
+++ murkmod.sh
@@ -166,58 +166,6 @@ set_sudo_perms() {
     fi
 }
 
-collect_analytics() {
-    hwid=$(crossystem.old hwid)
-    
-    if [ ! -f /mnt/stateful_partition/murkmod/method_collected ]; then
-        read -r -p "Did you use sh1mmer to unenroll this device? Reply with N if you used a different method. [y/N] " choice
-        case "$choice" in
-            y | Y) used_sh1mmer="yes" && touch /mnt/stateful_partition/murkmod/method_collected ;;
-            *) used_sh1mmer="no" && touch /mnt/stateful_partition/murkmod/method_collected ;;
-        esac
-        curl -X POST \
-             -H "Content-Type: application/json" \
-             -d "{\"hwid\":\"${hwid}\",\"method\":\"${used_sh1mmer}\"}" \
-             https://murkmod-analytics.besthaxer.repl.co/addmethod > /dev/null
-    fi
-    if [ ! -f /mnt/stateful_partition/murkmod/guide_collected ]; then
-        read -r -p "Did you use the Chromebook Exploit Guide (chromebook-guide.github.io) to fakemurk your device? [y/N] " choice
-        case "$choice" in
-            y | Y) used_guide="yes" && touch /mnt/stateful_partition/murkmod/guide_collected ;;
-            *) used_guide="no" && touch /mnt/stateful_partition/murkmod/guide_collected ;;
-        esac
-        curl -X POST \
-             -H "Content-Type: application/json" \
-             -d "{\"hwid\":\"${hwid}\",\"guide\":\"${used_guide}\"}" \
-             https://murkmod-analytics.besthaxer.repl.co/addguide > /dev/null
-    fi
-
-    fakemurk_version=$(cat /mnt/stateful_partition/fakemurk_version | base64 -w 0 || echo "")
-    murkmod_version=$(cat /mnt/stateful_partition/murkmod_version | base64 -w 0)
-    release_board=$(lsbval CHROMEOS_RELEASE_BOARD)
-    devicetype=$(lsbval DEVICETYPE)
-    auserver=$(lsbval CHROMEOS_AUSERVER)
-    chromeos_version=$(lsbval CHROMEOS_RELEASE_DESCRIPTION)
-    build_type=$(lsbval CHROMEOS_RELEASE_BUILD_TYPE)
-    chrome_milestone=$(lsbval CHROMEOS_RELEASE_CHROME_MILESTONE)
-    release_track=$(lsbval CHROMEOS_RELEASE_TRACK)
-    curl -X POST \
-         -H "Content-Type: application/json" \
-         -d "{\"murkmod_version\":\"${murkmod_version}\",\"fakemurk_version\":\"${fakemurk_version}\",\"hwid\":\"${hwid}\",\"release_board\":\"${release_board}\",\"devicetype\":\"${devicetype}\",\"auserver\":\"${auserver}\",\"cros_version\":\"${chromeos_version}\",\"build_type\":\"${build_type}\",\"chrome_milestone\":\"${chrome_milestone}\",\"release_track\":\"${release_track}\"}" \
-         https://murkmod-analytics.besthaxer.repl.co/analytics > /dev/null
-    echo "Analytics collected."
-}
-
-get_analytics_permission() {
-    echo "Analytics are completely anonymous - based on HWID only. It only collects basic information about your device and requires only minimal information on your part."
-    echo "Although I would greatly appreciate it if you opt in, this is completely optional and you can opt out at any time by deleting /mnt/stateful_partition/murkmod/analytics_opted_in."
-    read -r -p "Opt-in to analytics? [y/N] " choice
-    case "$choice" in
-        y | Y) collect_analytics && touch /mnt/stateful_partition/murkmod/analytics_opted_in ;;
-        *) echo "Opting out of analytics." && touch /mnt/stateful_partition/murkmod/analytics_opted_out ;;
-    esac
-}
-
 set_cros_debug() {
     sed -i "s/\(cros_debug=\).*/\11/" /usr/bin/crossystem
 }
@@ -258,13 +206,6 @@ murkmod() {
         set_sudo_perms
         echo "Setting crossystem cros_debug..."
         set_cros_debug
-        if [ ! -f /mnt/stateful_partition/murkmod/analytics_opted_in ]; then
-            if [ ! -f /mnt/stateful_partition/murkmod/analytics_opted_out ]; then
-                get_analytics_permission
-            fi
-        else
-            collect_analytics
-        fi
     fi
     read -n 1 -s -r -p "Done. If cros_debug was enabled for the first time, a reboot may be required. Press any key to exit."
     exit
